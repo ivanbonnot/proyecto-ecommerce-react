674c211db54c09018d9177386a5e94d7e61af03c
Ivan Bonnot <ivanbonnot@gmail.com>
2022-10-31 19:07:18 -0300

    agregado gh-pages

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-
 import { Link } from 'react-router-dom';
-
 import styled from "styled-components";
 import Cart from "../CartWidget/CartWidget";
-
 
 
 function NavBar() {
